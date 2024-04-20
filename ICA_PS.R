d43b42b686f5b0e185902efd73ece44723b78d1c
Harshan Mehra <bt23663@qmul.ac.uk>
2024-04-20 21:19:12 +0100

    some changes to ICA analysis

--- ICA_PS.R
+++ ICA_PS.R
@@ -8,12 +8,17 @@ library(fastICA)
 counts <- assay(PSdata)
 
 # Transpose the count matrix if necessary (ICA usually operates on samples as rows)
-counts <- t(counts)
+#counts <- t(counts)
 
 # Perform ICA
-ica_result <- fastICA(counts, n.comp = 2)  # Adjust the number of components as needed
+ica_result <- fastICA(counts, n.comp = 10,alg.typ = "parallel")  # Adjusting the number of components as needed
 
 # Access the independent components (ICs)
 independent_components <- ica_result$S
 
 # Optionally, you can visualize the ICs or perform further analysis
+# Example heatmap of the first independent component
+library(pheatmap)
+pheatmap(independent_components, scale = "column", cluster_rows = TRUE)
+
+dim(independent_components)
